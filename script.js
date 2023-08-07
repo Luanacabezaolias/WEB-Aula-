03a66ef101d8ed5c5b834fa41e955cfb5a37e60e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-07 11:43:39 -0300

    aula

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ const exemplo2 = (num)=>{
 const exemplo3 = (num)=>{return num * num;}
 console.log(exemplo(20))
 
+
+function inserir(){
+    let numero = document.getElementById("posicao").value - 1
+    let novo = document.getElementById("novo").value
+    document.getElementsByClassName("colocado")[numero].innerHTML = novo
+}
+
+function mostrar(){
+    let numero = document.getElementById("numero").value -1
+    let mes = document.getElementsByTagName("li")[numero].innerHTML
+    document.getElementById("resultado").innerHTML = mes
+}
+
+//document.querySelector("#btnMsg").onclick = ()=>alert("testes")
+
+document.querySelector("#btnMsg").addEventListener('keyDown', function(){
+    alert("teste novamente")
+})
